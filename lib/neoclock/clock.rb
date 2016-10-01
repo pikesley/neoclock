e8fde2e891325fd79c920d58911fa8a49f6859f4
pikesley <github@orgraphone.org>
2016-10-01 22:56:28 +0100

    Smooth hours

--- lib/neoclock/clock.rb
+++ lib/neoclock/clock.rb
@@ -5,6 +5,9 @@ module Neoclock
     def initialize options = {}
       @config = Config.instance.config
 
+      @figure = @config.colours['figure']
+      @ground = @config.colours['ground']
+
       @total_size = @config.minutes['pins'] + @config.hours['pins']
 
       @rings = PixelPi::Leds.new \
@@ -20,17 +23,23 @@ module Neoclock
       dt = DateTime.now
       @lights = []
 
-      lit = [
-        ((@config.minutes['pins'] / 60.0) * dt.minute).to_i,
-        (dt.hour % 12) + @config.minutes['pins']
-      ]
+      minutes = ((@config.minutes['pins'] / 60.0) * dt.minute).to_i
+      hours = (dt.hour % 12) + @config.minutes['pins']
 
-      @total_size.times do |i|
-        if lit.include? i
-          @lights.push @config.colours['figure']
+      step = 0
+      while step < @total_size do
+        case step
+        when minutes
+          @lights[step] = @figure
+        when hours
+          @lights[step] = @figure
+          @lights[step + 1] = Neoclock.blender @ground, @figure, dt.minute
+          step += 1
         else
-          @lights.push @config.colours['ground']
+          @lights[step] = @ground
         end
+
+        step += 1
       end
 
       @lights.each_with_index do |colour, i|
