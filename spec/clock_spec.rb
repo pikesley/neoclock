c7bc0b123fd03b36fc9f08a306913ad35141ac52
pikesley <github@orgraphone.org>
2016-10-01 23:44:20 +0100

    Blurred minutes

--- spec/clock_spec.rb
+++ spec/clock_spec.rb
@@ -7,14 +7,14 @@ module Neoclock
         Timecop.freeze DateTime.parse '13:00' do
           clock.time
           expect(clock.lights).to eq [
-            [255, 0, 0], [0, 0, 255], [0, 0, 255],
+            [255, 0, 0], [127, 0, 127], [0, 0, 255],
             [0, 0, 255], [0, 0, 255], [0, 0, 255],
             [0, 0, 255], [0, 0, 255], [0, 0, 255],
             [0, 0, 255], [0, 0, 255], [0, 0, 255],
             [0, 0, 255], [0, 0, 255], [0, 0, 255],
             [0, 0, 255], [0, 0, 255], [0, 0, 255],
             [0, 0, 255], [0, 0, 255], [0, 0, 255],
-            [0, 0, 255], [0, 0, 255], [0, 0, 255],
+            [0, 0, 255], [0, 0, 255], [127, 0, 127],
 
             [0, 0, 255], [255, 0, 0], [0, 0, 255],
             [0, 0, 255], [0, 0, 255], [0, 0, 255],
@@ -33,6 +33,61 @@ module Neoclock
             [0, 0, 255], [0, 0, 255], [0, 0, 255],
             [255, 0, 0], [0, 0, 255], [0, 0, 255]
           ]
+
+          expect(clock.lights[11..13]).to eq [
+            [127, 0, 127],
+            [255, 0, 0],
+            [127, 0, 127]
+          ]
+        end
+      end
+
+      context 'edge cases' do
+        it 'handles on-the-hour' do
+          Timecop.freeze DateTime.parse '09:00' do
+            clock.time
+            expect(clock.lights[23]).to eq [127, 0, 127]
+            expect(clock.lights[0]).to eq [255, 0, 0]
+            expect(clock.lights[1]).to eq [127, 0, 127]
+          end
+        end
+      end
+
+      context 'minute block' do
+        it 'handles the easy case' do
+          expect(
+            Clock.minute_block 12, [255, 0, 0], [0, 0, 255]
+          ).to eq (
+            {
+              11 => [127, 0, 127],
+              12 => [255, 0, 0],
+              13 => [127, 0, 127]
+            }
+          )
+        end
+
+        it 'handles the on-the-hour case' do
+          expect(
+            Clock.minute_block 0, [255, 0, 0], [0, 0, 255]
+          ).to eq (
+            {
+              23 => [127, 0, 127],
+              0  => [255, 0, 0],
+              1  => [127, 0, 127]
+            }
+          )
+        end
+
+        it 'handles the just-before-the-hour case' do
+          expect(
+            Clock.minute_block 23, [255, 0, 0], [0, 0, 255]
+          ).to eq (
+            {
+              22 => [127, 0, 127],
+              23 => [255, 0, 0],
+              0  => [127, 0, 127]
+            }
+          )
         end
       end
     end
