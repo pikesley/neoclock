a0fdddb13f7cb56c1161d86fa693ac4b8bd746d7
pikesley <github@orgraphone.org>
2016-10-01 17:48:18 +0100

    Blend colours

--- lib/neoclock.rb
+++ lib/neoclock.rb
@@ -11,6 +11,23 @@ require 'neoclock/config'
 # http://www.perbang.dk/rgbgradient/
 
 module Neoclock
+  def self.blender start, stop, sixtieth
+    c = []
+    start.each_with_index do |component, i|
+      c.push intersperse(component, stop[i], sixtieth)
+    end
+    c
+  end
+
+  def self.intersperse first, second, sixtieth
+    lower, higher = first, second
+    if second < first
+      lower, higher = second, first
+      sixtieth = 60 - sixtieth
+    end
+    ((higher - lower) / 60.0 * sixtieth).to_i + lower
+  end
+
   def self.alg length, angle
     ((length / 360.0) * angle)
   end
