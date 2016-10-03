faf7cc7edca0355bc7bf3fc432567a4522083f2d
pikesley <github@orgraphone.org>
2016-10-03 23:22:32 +0100

    Oops

--- lib/neoclock/tricks.rb
+++ lib/neoclock/tricks.rb
@@ -3,6 +3,7 @@ module Neoclock
     def self.shuffle iterations = 64
       iterations.times do
         self.time DateTime.strptime(Random.rand(86400).to_s, '%s')
+        sleep 0.05
       end
 
       self.time
